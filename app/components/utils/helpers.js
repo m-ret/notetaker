745805312ec1d3326f8fd6932775e562eadee88b
Maketroli <maketroli@gmail.com>
2015-07-25 00:46:25 -0600

    commit

---
+++ app/components/utils/helpers.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+function getRepos(username) {
+	return axios.get(`https://api.github.com/users/${username}/repos`);
+};
+
+function getUserInfo (username) {
+	return axios.get(`https://api.github.com/users/${username}`);
+}
+
+var promiseObj = getRepos('tylermcginnis');
+promiseObj.then(function(data) {
+	console.log(data);
+});
+
+var helpers = {
+	getGithubInfo: function(username) {
+		return axios.all([getRepos(username), getUserInfo(username)])
+			.then((arr) => {
+				return {
+					repos: arr[0].data,
+					bio: arr[1].data					
+				}
+			});
+	}
+};
+
+export default helpers;
